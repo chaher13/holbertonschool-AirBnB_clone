cf4683e4a6f35a46950e684abd335e62a0a90763
GaetanLecuyer <[200~6229@holbertonstudents.com~>
2023-07-06 18:13:24 +0200

    go

--- models/base_model.py
+++ models/base_model.py
@@ -1,39 +1,67 @@
 #!/usr/bin/python3
-from datetime import datetime
+"""
+This module contains the BaseModel class.
+"""
 import uuid
+from datetime import datetime
 import models
 
 
 class BaseModel:
+    """
+    Base class that defines common attributes and methods for other classes.
+    """
+
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of BaseModel.
+        If it's a new instance (not from a dictionary representation),
+        adds a call to the new(self) method on storage.
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
         if kwargs:
-            # Set attributes from kwargs dictionary
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime
+                                (value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
         else:
-            # Create new instance with id, created_at, and updated_at
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)  # Add the instance to storage
+            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+        """
+        Returns a string representation of the BaseModel instance.
+        Format: "[<class name>] (<self.id>) <self.__dict__>"
+        """
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        Calls the save(self) method of storage.
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary representation of the instance.
+        Converts the BaseModel instance to a dictionary representation.
+        - Creates a copy of the instance's __dict__.
+        - Adds the '__class__' key with the class name of the object.
+        - Converts created_at and updated_at to string objects in ISO format.
+        Returns:
+            A dictionary containing all keys/values of the instance.
         """
-        obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-        return obj_dict
-
-    def save(self):
-        self.updated_at = datetime.now()
-        models.storage.save()
+        data = self.__dict__.copy()
+        data['__class__'] = self.__class__.__name__
+        data['created_at'] = self.created_at.isoformat()
+        data['updated_at'] = self.updated_at.isoformat()
+        return data
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,55 +1,78 @@
 #!/usr/bin/python3
+"""
+This module contains the FileStorage class.
+"""
 import json
+from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
-from models.amenity import Amenity
+from models.state import State
 from models.city import City
+from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
-from models.state import State
 
 
 class FileStorage:
+    """
+    Class that serializes instances to a JSON file
+        and deserializes JSON file to instances.
+    """
+
     __file_path = "file.json"
     __objects = {}
 
+    class_mapping = {
+        "BaseModel": BaseModel,
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "Review": Review
+    }
+
     def all(self):
+        """
+        Returns the dictionary __objects.
+        Returns:
+            The dictionary __objects.
+        """
         return self.__objects
 
     def new(self, obj):
+        """
+        Sets the object in __objects with the key <obj class name>.id.
+        Args:
+            obj: The object to be set in __objects.
+        """
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        serialized_objects = {}
+        """
+        Serializes __objects to the JSON file (__file_path).
+        """
+        obj_dict = {}
         for key, obj in self.__objects.items():
-            serialized_objects[key] = obj.to_dict()
-
+            obj_dict[key] = obj.to_dict()
         with open(self.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
+            json.dump(obj_dict, file)
 
     def reload(self):
-        class_mapping = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "Place": Place,
-        "State": State,
-        "City": City,
-        "Amenity": Amenity,
-        "Review": Review,
-    }
-
+        """
+        Deserializes the JSON file to __objects (only if the file exists).
+        If the file doesn't exist, no exception should be raised.
+        """
         try:
             with open(self.__file_path, 'r') as file:
                 obj_dict = json.load(file)
                 for key, obj_attr in obj_dict.items():
                     class_name, obj_id = key.split('.')
                     obj_attr["__class__"] = class_name
-                    if class_name in class_mapping:
-                        class_obj = class_mapping[class_name]
+                    if class_name in __class__.class_mapping:
+                        class_obj = __class__.class_mapping[class_name]
                         obj = class_obj(**obj_attr)
                         self.__objects[key] = obj
         except FileNotFoundError:
-            pass
-        except json.JSONDecodeError:
             pass
